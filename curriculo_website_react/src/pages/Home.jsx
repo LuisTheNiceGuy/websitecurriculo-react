0a27076f50dfe76f4cdc444dc6c1dbf3975fd127
LuisGus <luisgus16@outlook.com.br>
2023-07-10 01:21:38 -0300

    add typewrite na home e alteraçao no titulo navbar

--- curriculo_website_react/src/pages/Home.jsx
+++ curriculo_website_react/src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import foto_eu from './eu.jpg';
 import "./Home.css";
-
+import {Typewriter} from 'react-simple-typewriter'
 
 function Home() {
   
     return ( 
         <>
+          <div className='container'>
             <img className='foto_eu' src={foto_eu} alt="Luis Gustavo CS"/>
-            <h1>Hi, Im Luis Gustavo</h1>
+           <span className="apresentacao">
+              <Typewriter 
+                words={["Hi, i'm Luis Gustavo"]}
+                cursor
+                
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                
+              />
+           </span>
+                  
+          </div>
         </>
      );
 }
